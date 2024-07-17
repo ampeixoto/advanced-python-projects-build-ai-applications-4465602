f2c4e7a642dba27a59705a6f2e0fb15250b02e97
ampeixoto <ampeixoto@users.noreply.github.com>
2024-07-17 10:57:42 +0000

    01 Chatbot with sentiment analysis

--- Begin/CH_1_NLP_ChatBot.py
+++ Begin/CH_1_NLP_ChatBot.py
@@ -1,35 +1,32 @@
 # Importing TextBlob to help the chatbot understand language nuances.
 from textblob import TextBlob
 
-
 # Defining the ChatBot class for interaction.
 class ChatBot:
     def __init__(self):
         # Initializing the sentiment analysis tool.
         self.sentiment_analyzer = TextBlob("")
-
+            # Analyzing the sentiment of the user's message.
     def start_chat(self):
-        print("ChatBot: Hi, how can I help you?")
+        print("Chatbot: Hi, how can I help you today?")
         while True:
             user_message = input("You: ").strip()
-
-            # Analyzing the sentiment of the user's message.
+            
+            # Generating the chatbot's response based on sentiment.
             self.sentiment_analyzer = TextBlob(user_message)
             sentiment_score = self.sentiment_analyzer.sentiment.polarity
 
-            # Generating the chatbot's response based on sentiment.
-            if sentiment_score > 0:
-                chatbot_message = f"ChatBot: That's great to hear! \n Sentiment score: {sentiment_score}\n"
-            elif sentiment_score < 0:
-                chatbot_message = f"ChatBot: I'm sorry to hear that. \n Sentiment score: {sentiment_score}\n"
-            else:
-                chatbot_message = f"ChatBot: Hmm, I see. \n Sentiment score: {sentiment_score}\n"
-
             # Printing the chatbot's response and sentiment.
+            if(sentiment_score > 0):
+                chatbot_message = f"Chatbot: That's great to hear! \n Sentiment Score: {sentiment_score}"
+            elif(sentiment_score < 0):
+                chatbot_message = f"Chatbot: I'm sorry to hear that. Would you like me to transfer you to a live agent? \n Sentiment Score: {sentiment_score}"
+            else:
+                chatbot_message = f"Chatbot: I see.\n Sentiment Score: {sentiment_score}"
+
             print(chatbot_message)
 
-
-if __name__ == "__main__":
     # Creating the chatbot and starting the chat loop.
+if __name__ == "__main__":
     chatbot = ChatBot()
     chatbot.start_chat()
